1e6d8ed75d5b1634f5ec8a68ed8ef5d7cb6426c1
tanxingzhong <zetter358@gmail.com>
2025-01-18 23:59:46 +0800

    test2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final BobAndSteve bob = new BobAndSteve();
+        //test
         bob.greeting();
         while(scanner.hasNext()) {
             bob.commands(scanner.nextLine());
