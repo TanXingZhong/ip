f5ee701aaf23b510a5aec4f62596200373fbae65
tanxingzhong <zetter358@gmail.com>
2025-01-19 00:47:33 +0800

    Mark as Done

--- src/main/java/bobAndSteve/BobAndSteve.java
+++ src/main/java/bobAndSteve/BobAndSteve.java
@@ -4,24 +4,31 @@ import java.util.List;
 
 public class BobAndSteve {
     private final String name = "Bob and Steve";
-    private List<String> lst = new ArrayList<>();
+    private final Task task;
+    private final Bye bye;
 
     public BobAndSteve() {
-
+        this.bye = new Bye();
+        this.task = new Task();
     }
 
-    public void commands(String command) {
-        if (command.equals("bye")) {
-            bye();
-            System.exit(0);
-        } else if (command.equals("list")) {
-            list();
-        } else if (command.equals("blah")) {
-            System.out.println("   ____________________________________________________________\n" +
-                    "     blah\n" +
-                    "    ____________________________________________________________");
-        } else {
-            addList(command);
+    public void commands(String input) {
+        String[] split = input.split(" ");
+        String command = split[0];
+        switch (command) {
+            case "bye" -> {
+                bye.end();
+            }
+            case "list" -> task.list();
+            case "mark" -> {
+                int pos = Integer.parseInt(split[1]);
+                task.mark(pos);
+            }
+            case "unmark" ->{
+                int pos = Integer.parseInt(split[1]);
+                task.unmark(pos);
+            }
+            default -> task.addList(input);
         }
     }
 
@@ -33,22 +40,6 @@ public class BobAndSteve {
         System.out.println(greeting);
     }
 
-    public void bye() {
-        String bye = "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(bye);
-    }
-
-    public void addList(String task) {
-        lst.add(task);
-        System.out.println("added: " + task);
-    }
-
-    public void list() {
-        for (int i = 0; i < lst.size(); i++) {
-            System.out.println(i + 1 + ". " + lst.get(i));
-        }
-    }
 
     @Override
     public String toString() {
---
+++ src/main/java/bobAndSteve/Bye.java
@@ -0,0 +1,10 @@
+package bobAndSteve;
+
+public class Bye {
+    public void end() {
+        String bye ="Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(bye);
+        System.exit(0);
+    }
+}
