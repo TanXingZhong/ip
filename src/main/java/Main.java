afe17cb708878cd475593e362b179d4bbd986404
tanxingzhong <zetter358@gmail.com>
2025-01-18 22:47:10 +0800

    Echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import bobAndSteve.BobAndSteve;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         final BobAndSteve bob = new BobAndSteve();
         bob.greeting();
+        while(scanner.hasNext()) {
+            bob.commands(scanner.next());
+        };
     }
 }
--- src/main/java/bobAndSteve/BobAndSteve.java
+++ src/main/java/bobAndSteve/BobAndSteve.java
@@ -1,21 +1,41 @@
 package bobAndSteve;
 
 public class BobAndSteve {
-
     private final String name = "Bob and Steve";
 
     public BobAndSteve() {
 
     }
 
+    public void commands(String command) {
+        if(command.equals("bye")) {
+            bye();
+            System.exit(0);
+        } else if(command.equals("list")) {
+            System.out.println("   ____________________________________________________________\n" +
+                    "     list\n" +
+                    "    ____________________________________________________________");
+        } else if(command.equals("blah")) {
+            System.out.println("   ____________________________________________________________\n" +
+                    "     blah\n" +
+                    "    ____________________________________________________________");
+        } else {
+            System.out.println("Unknown command");
+        }
+    }
+
     public void greeting() {
         String greeting = "____________________________________________________________\n" +
-                "Hello! I'm " + name + " \n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                          "Hello! I'm " + name + " \n" +
+                          "What can I do for you?\n" +
+                          "____________________________________________________________\n";
         System.out.println(greeting);
+    }
+
+    public void bye() {
+        String bye ="Bye. Hope to see you again soon!\n" +
+                    "____________________________________________________________\n";
+        System.out.println(bye);
     }
 
     @Override
