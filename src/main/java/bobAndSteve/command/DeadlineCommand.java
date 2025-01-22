6cce897f6b41b97a60a55ca7ce9cf06467243796
tanxingzhong <zetter358@gmail.com>
2025-01-22 19:52:43 +0800

    Fixed Invalid command inputs

--- src/main/java/bobAndSteve/command/DeadlineCommand.java
+++ src/main/java/bobAndSteve/command/DeadlineCommand.java
@@ -13,16 +13,20 @@ public class DeadlineCommand extends Command {
     private final String by;
 
     public DeadlineCommand(String input) throws InvalidCommandFormatException {
-        String[] split = input.split(" ", 2);
-        if (split[1].isEmpty()) {
+        try {
+            String[] split = input.split(" ", 2);
+            if (split[1].isEmpty()) {
+                throw new InvalidCommandFormatException("Invalid format. Use: <task> /by <deadline>");
+            }
+            String[] by = split[1].split("/by", 2);
+            if (by.length < 2 || by[0].trim().isEmpty() || by[1].trim().isEmpty()) {
+                throw new InvalidCommandFormatException("Invalid format. Use: <task> /by <deadline>");
+            }
+            this.description = by[0].trim();
+            this.by = by[1].trim();
+        } catch (ArrayIndexOutOfBoundsException e) {
             throw new InvalidCommandFormatException("You must specify the task and deadline in the format: <task> /by <deadline>");
         }
-        String[] by = split[1].split("/by", 2);
-        if (by.length < 2 || by[0].trim().isEmpty() || by[1].trim().isEmpty()) {
-            throw new InvalidCommandFormatException("Invalid format. Use: <task> /by <deadline>");
-        }
-        this.description = by[0].trim();
-        this.by = by[1].trim();
     }
 
     @Override
